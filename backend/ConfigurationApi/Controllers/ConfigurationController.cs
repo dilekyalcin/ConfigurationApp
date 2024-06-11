3240905f2fd22473a29b0edcab98265b94da51a6
Dilek <dilekyal1@gmail.com>
2024-06-12 00:38:25 +0300

    created backend web api

---
+++ backend/ConfigurationApi/Controllers/ConfigurationController.cs
@@ -0,0 +1,74 @@
+﻿using Business.Abstract;
+using Entity.Concrete;
+using Entity.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WebConfigurationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigurationController : ControllerBase
+    {
+        IConfigurationAppService _configurationAppService;
+
+        public ConfigurationController(IConfigurationAppService configurationAppService)
+        {
+            _configurationAppService = configurationAppService;
+        }
+
+        [HttpGet("{applicationName}")]
+        public ActionResult RefreshConfigurations(string applicationName)
+        {
+            _configurationAppService.RefreshConfigurations(applicationName);
+            return Ok("Configurations refreshed successfully");
+        }
+
+        [HttpGet("getvaluebykey/{key}")]
+        public IActionResult GetValueByKey(string key)
+        {
+            var value = _configurationAppService.GetValue<string>(key);
+            var result = new { key = value };
+            return new JsonResult(result);
+        }
+
+
+        [HttpGet("getAllConfigurations")]
+        public IActionResult GetAllConfigurations()
+        {
+            var configurations = _configurationAppService.GetAllConfigurations();
+            return new JsonResult(configurations);
+        }
+
+        [HttpPost]
+        public IActionResult AddConfiguration([FromBody] ConfigurationDto configDto)
+        {
+            var config = new t_Configurations
+            {
+                ApplicationName = configDto.ApplicationName,
+                Name = configDto.Name,
+                Value = configDto.Value,
+                Type = configDto.Type,
+                IsActive = configDto.IsActive
+            };
+
+            _configurationAppService.AddConfiguration(config);
+            return CreatedAtAction(nameof(RefreshConfigurations), new { applicationName = config.ApplicationName }, config);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateConfiguration([FromBody] t_Configurations config)
+        {
+            _configurationAppService.UpdateConfiguration(config);
+            return Ok("Configuration updated successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteConfiguration(int id)
+        {
+            _configurationAppService.DeleteConfiguration(id);
+            return Ok("Configuration deleted successfully");
+        }
+    }
+}
---
+++ backend/DataAccess/Concrete/EntityFramework/EfConfigurationDal.cs
@@ -0,0 +1,101 @@
+﻿using DataAccess.Abstract;
+using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfConfigurationDal : IConfigurationDal
+    {
+
+        //public Dictionary<string, (object Value, Type Type)> GetConfigurations(string applicationName)
+        //{
+        //    using (var context = new ConfigurationContext())
+        //    {
+        //        var configurations = new Dictionary<string, (object, Type)>();
+        //        var configs = context.t_Configurations
+        //                              .Where(c => c.ApplicationName == applicationName && c.IsActive)
+        //                              .ToList();
+
+        //        foreach (var config in configs)
+        //        {
+        //            var type = Type.GetType(config.Type);
+        //            var value = Convert.ChangeType(config.Value, type);
+        //            configurations[config.Name] = (value, type);
+        //        }
+
+        //        return configurations;
+        //    }
+        //}
+        public List<t_Configurations> GetAllConfigurations()
+        {
+            using (var context = new ConfigurationContext())
+            {
+                var configs = context.t_Configurations.Select(p => new t_Configurations()
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    ApplicationName = p.ApplicationName,
+                    IsActive = p.IsActive, 
+                    Type = p.Type,
+                    Value = p.Value
+                });
+
+                return configs.ToList();
+            }
+        }
+
+
+        public T GetValue<T>(string key)
+        {
+            using (var context = new ConfigurationContext())
+            {
+                var config = context.t_Configurations.FirstOrDefault(c => c.Name == key);
+                if (config != null)
+                {
+                    return (T)Convert.ChangeType(config.Value, typeof(T));
+                }
+                else
+                {
+                    throw new KeyNotFoundException("Configuration key not found.");
+                }
+            }
+        }
+
+  
+
+        public void AddConfiguration(t_Configurations config)
+        {
+            using (var context = new ConfigurationContext())
+            {
+                context.t_Configurations.Add(config);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateConfiguration(t_Configurations config)
+        {
+            using (var context = new ConfigurationContext())
+            {
+                context.t_Configurations.Update(config);
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteConfiguration(int id)
+        {
+            using (var context = new ConfigurationContext())
+            {
+                var config = context.t_Configurations.FirstOrDefault(c => c.Id == id);
+                if (config != null)
+                {
+                    context.t_Configurations.Remove(config);
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
